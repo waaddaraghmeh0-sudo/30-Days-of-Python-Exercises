a44f89f37ea803cbdaab96b87a9dd81631e5ac05
waaddaraghmeh0-sudo <waaddaraghmeh0@gmail.com>
2026-06-23 17:56:10 +0300

    Save local changes before pull

---
+++ Day_01/day01exercisessolution.py
@@ -0,0 +1,79 @@
+"""
+-level 1-
+
+Q1: do the following operations. The operands are 3 and 4.
+addition(+)
+subtraction(-)
+multiplication(*)
+modulus(%)
+division(/)
+exponential(**)
+floor division operator(//)
+"""
+
+print (4+3)
+print (4-3)
+print (4*3)
+print (4%3)
+print (4/3)
+print (4**3)
+print (4//3)
+
+"""
+Q2: Write strings. The strings are the following:
+Your name
+Your family name
+Your country
+I am enjoying 30 days of python
+"""
+
+print ('waad')
+print ('daraghmeh')
+print ('palestine')
+print ('I am enjoying 30 days of python')
+
+
+"""
+Q3: Check the data types of the following data:
+10
+9.8
+3.14
+4 - 4j
+['Asabeneh', 'Python', 'Finland']
+Your name
+Your family name
+Your country
+"""
+
+print (type(10))
+print (type(9.8))
+print (type(3.14))
+print (type(4-4j))
+print (type(['Asabeneh','Python','Finland']))
+print (type('waad'))
+print (type('daraghmeh'))
+print (type('palestine'))
+
+
+"""
+-level 2-
+
+Q1:Write an example for different Python data types such as Number(Integer, Float, Complex), 
+String, Boolean, List, Tuple, Set and Dictionary.
+"""
+
+print (67)
+print (4.6)
+print (1+6j)
+print ("skibidi")
+print (True)
+print ([2,4,6,8])
+print ((1,3,5,9))
+print ({4,4,4})
+print ({"name": "waad", "country" : "palestine"})
+
+
+"""
+Q2: Find an Euclidean distance between (2, 3) and (10, 8)
+"""
+print ((8**2 + 5**2) **0.5)

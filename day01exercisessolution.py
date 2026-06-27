77135b7bda1cb0fed65f32e8dddb182e31fcff01
waaddaraghmeh0-sudo <waaddaraghmeh0@gmail.com>
2026-06-27 16:52:28 +0300

    day 01

--- day01exercisessolution.py
+++ day01exercisessolution.py
@@ -1,15 +1,14 @@
-"""
--level 1-
 
-Q1: do the following operations. The operands are 3 and 4.
-addition(+)
-subtraction(-)
-multiplication(*)
-modulus(%)
-division(/)
-exponential(**)
-floor division operator(//)
-"""
+#-level 1-
+
+#Q1: do the following operations. The operands are 3 and 4.
+#addition(+)
+#subtraction(-)
+#multiplication(*)
+#modulus(%)
+#division(/)
+#exponential(**)
+#floor division operator(//)
 
 print (4+3)
 print (4-3)
@@ -19,13 +18,13 @@ print (4/3)
 print (4**3)
 print (4//3)
 
-"""
-Q2: Write strings. The strings are the following:
-Your name
-Your family name
-Your country
-I am enjoying 30 days of python
-"""
+
+#Q2: Write strings. The strings are the following:
+#Your name
+#Your family name
+#Your country
+#I am enjoying 30 days of python
+
 
 print ('waad')
 print ('daraghmeh')
@@ -33,17 +32,17 @@ print ('palestine')
 print ('I am enjoying 30 days of python')
 
 
-"""
-Q3: Check the data types of the following data:
-10
-9.8
-3.14
-4 - 4j
-['Asabeneh', 'Python', 'Finland']
-Your name
-Your family name
-Your country
-"""
+
+#Q3: Check the data types of the following data:
+#10
+#9.8
+#3.14
+#4 - 4j
+#['Asabeneh', 'Python', 'Finland']
+#Your name
+#Your family name
+#Your country
+
 
 print (type(10))
 print (type(9.8))
@@ -55,12 +54,12 @@ print (type('daraghmeh'))
 print (type('palestine'))
 
 
-"""
--level 2-
 
-Q1:Write an example for different Python data types such as Number(Integer, Float, Complex), 
-String, Boolean, List, Tuple, Set and Dictionary.
-"""
+#-level 2-
+
+#Q1:Write an example for different Python data types such as Number(Integer, Float, Complex), 
+#String, Boolean, List, Tuple, Set and Dictionary.
+
 
 print (67)
 print (4.6)
@@ -73,7 +72,7 @@ print ({4,4,4})
 print ({"name": "waad", "country" : "palestine"})
 
 
-"""
-Q2: Find an Euclidean distance between (2, 3) and (10, 8)
-"""
+
+#Q2: Find an Euclidean distance between (2, 3) and (10, 8)
+
 print ((8**2 + 5**2) **0.5)
